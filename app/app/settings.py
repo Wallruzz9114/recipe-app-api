9caf9ecbc0f6cf325bec9c556b69b908203479f2
Jose Pinto <pintojose.benedicto2@gmail.com>
2019-12-15 13:50:59 -0700

    Set up postgres

--- app/app/settings.py
+++ app/app/settings.py
@@ -81,7 +81,6 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
